1a5441a947dc63b437f72c0fe516622a3031860e
FabianGutendorf <fgutendorf@aol.com>
2017-02-05 14:45:54 +0100

    Output with colors and hit rate system, unoptimized, still hardcoded

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+mod output;
+
 fn main() {
-    println!("Hello, world!");
+    //Only for testing
+    let x = "Star Wars - The Last Generic";
+    let xx = "Die Generic 3";
+    let xxx = "Die Telefloppies";
+    let mut act = Vec::new();
+    act.push("Bruce Lee");
+    act.push("Jason Statham");
+    act.push("Cool Cat");
+    let mut actTwo = Vec::new();
+    actTwo.push("Irgendwer");
+    actTwo.push("Geralt von Riva");
+    let mut actThree = Vec::new();
+    actThree.push("The Rock");
+    actThree.push("james Bond");
+    let yearOne = "1204";
+    let yearTwo = "2013";
+    let yearThree = "2016";
+    let genOne = "Comedy";
+    let genTwo = "Mystery";
+    let genThree = "Horror";
+    let y = output::buildRank(6);
+    let z = output::buildRank (10);
+    let zz = output::buildRank(2);
+    let mut input = Vec::new();
+    input.push("Comedy");
+    input.push("Cool Cat");
+    let mut inputTwo = Vec::new();
+    inputTwo.push("**");
+    inputTwo.push("Die Telefloppies");
+    inputTwo.push("Darth Vader");
+
+    output::outputResult(x, &y, &act, genOne, yearOne, &input);
+    output::outputResult(xx, &z, &actTwo, genTwo, yearTwo, &input);
+    output::outputResult(xxx, &zz, &actThree, genThree, yearThree, &inputTwo);
 }
--- src/output/mod.rs
+++ src/output/mod.rs
@@ -1,45 +1,10 @@
-// extern crate term_painter;
+extern crate term_painter;
 
-//use term_painter::ToStyle;
-//use term_painter::Color::*;
-//use term_painter::Attr::*;
+use self::term_painter::ToStyle;
+use self::term_painter::Color::*;
+use self::term_painter::Attr::*;
 
-fn main() {
-    //Only for testing
-    let x = "Star Wars - The Last Generic";
-    let xx = "Die Generic 3";
-    let mut act = Vec::new();
-    act.push("Bruce Lee");
-    act.push("Jason Statham");
-    act.push("Cool Cat");
-    let mut actTwo = Vec::new();
-    actTwo.push("Irgendwer");
-    actTwo.push("Geralt von Riva");
-    let yearOne = 1204;
-    let yearTwo = 2013;
-    let genOne = "Comedy";
-    let genTwo = "Mystery";
-    let y = buildRank(6);
-    let z = buildRank (10);
-
-    outputResult(x, &y, &act, genOne, yearOne);
-    outputResult(xx, &z, &actTwo, genTwo, yearTwo);
-}
-
-fn buildRank(x: u8) -> Vec<String> {
-    /*
-    let mut rank = String::from("(");
-    for i in 0..10-x {
-        &rank.push('-');
-    }
-    for i in 0..x {
-        &rank.push('*');
-    }
-
-    rank.push(')');
-    rank
-    **/
-
+pub fn buildRank(x: u8) -> Vec<String> {
     let mut rank = Vec::new();
     rank.push(String::from("("));
     rank.push(String::new());
@@ -54,16 +19,55 @@ fn buildRank(x: u8) -> Vec<String> {
     rank
 }
 
-fn outputResult(x: &str, y: &Vec<String>, act: &Vec<&str>, gen: &str, year: u32) {
+pub fn outputResult(x: &str, y: &Vec<String>, act: &Vec<&str>, gen: &str, year: &str, input: &Vec<&str>) {
+    let mut colors = Vec::new();
+    for i in 0..3 + y.len() + act.len() {
+        colors.push(White);
+    }
+    let count = createHits(x, y, act, gen, year, input, &mut colors);
+    let hits = match count {
+        0|1|2|3|4|5|6|7|8|9 => 15,
+        100 => 17,
+        _ => 16,
+    };
     let lengthFilm = 25 + x.chars().count() + 12;
     let formatFilm = format!("{:-<1$}", "-", lengthFilm);
     print!("{}\n", formatFilm); //Header
-    print!("= Film: {}         Rank: {}{}{}{} =\n",x, y[0], y[1], y[2], y[3]); //Movie and Rank, longest
+    print!("= {} {}%{}=\n", Blue.paint("Hit Rate:"), Blue.paint(count), format!("{: <1$}", " ", lengthFilm - hits)); 
+    print!("= Film: {}         Rank: {}{}{}{} =\n",colors[0].paint(x), y[0], y[1], colors[3].paint(&y[2]), y[3]); //Movie and Rank, longest
     print!("= Main Actors:{}=\n", format!("{: <1$}", " ", lengthFilm - 15));
     for u in 0..act.len() {
-        print!("=  - {}{}=\n", act[u], format!("{: <1$}", " ", lengthFilm - act[u].chars().count() - 6));
+        print!("=  - {}{}=\n", colors[5+u].paint(&act[u]), format!("{: <1$}", " ", lengthFilm - act[u].chars().count() - 6));
     }
-    print!("= Genre: {}{} =\n", gen, format!("{: <1$}", " ", lengthFilm - 11 - gen.chars().count())); //Genre
-    print!("= Year: {}{} =\n", year, format!("{: <1$}", " ", lengthFilm - 14)); //Year
+    print!("= Genre: {}{} =\n", colors[5+act.len()].paint(gen), format!("{: <1$}", " ", lengthFilm - 11 - gen.chars().count())); //Genre
+    print!("= Year: {}{} =\n", colors[6+act.len()].paint(year), format!("{: <1$}", " ", lengthFilm - 14)); //Year
     print!("{}\n", formatFilm); //Bottom
+}
+
+fn createHits(x: &str, y: &Vec<String>, act: &Vec<&str>, gen: &str, year: &str, input: &Vec<&str>, 
+colors: &mut Vec<self::term_painter::Color>) -> usize {
+    let mut data = Vec::new();
+    let mut count = 0;
+    data.push(x);
+    for i in 0..4 {
+        data.push(&y[i]);
+    }
+    for i in 0..act.len() {
+        data.push(act[i]);
+    }
+    data.push(gen);
+    data.push(year);
+    colors[3] = Yellow;
+
+    for u in 0..input.len() {
+        for v in 0..data.len() {
+            if(data[v].eq(input[u])) {
+                colors[v] = Green;
+                count = count + 1;
+                break;
+            }
+        }
+    }
+
+    (count * 100)/input.len()
 }

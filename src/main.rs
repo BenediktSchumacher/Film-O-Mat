a5de124cc5c7d4cd6e68021afeee7679a12089f7
Benedikt Schumacher <bschumacher@uos.de>
2017-02-09 19:55:10 +0100

    implemented database creation

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ fn main() {
     // TODO: Pruefem, ob Datenbank vorhanden ist
     // Falls nein => Dateipfad erstellen, Datenbank anlegen und anschliessend fuellen
     // Falls ja => Datenbankverbindung herstellen, Anfrage bearbeiten
-    println!("{:?}", db_exists());
-    create_database();
+    if !db_exists() {
+        create_database();
+    }
 }

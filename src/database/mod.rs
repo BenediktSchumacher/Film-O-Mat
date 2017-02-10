2ae379fcb032ac7032a9e597b30de4c18f6c2320
Benedikt Schumacher <bschumacher@uos.de>
2017-02-10 11:56:37 +0100

    added creation of database-tables

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -25,11 +25,38 @@ pub fn create_database() {
     let conn = Connection::open(path_buf).unwrap();
 
     // Example of SQL-Statement
-    conn.execute("CREATE TABLE IF NOT EXISTS person (
+    conn.execute("CREATE TABLE IF NOT EXISTS movies (
                   id              INTEGER PRIMARY KEY,
                   name            TEXT NOT NULL,
-                  time_created    TEXT NOT NULL,
-                  data            BLOB
+                  year            TEXT NOT NULL,
+                  genre           TEXT NOT NULL,
+                  director        TEXT NOT NULL
+                  )",
+                 &[])
+        .unwrap();
+
+    conn.execute("CREATE TABLE IF NOT EXISTS actors (
+                  id              INTEGER PRIMARY KEY,
+                  name            TEXT NOT NULL
+                  )",
+                 &[])
+        .unwrap();
+
+    conn.execute("CREATE TABLE IF NOT EXISTS rankings (
+                  id              INTEGER PRIMARY KEY,
+                  movie_id        INTEGER,
+                  score           TEXT NOT NULL,
+                  number          INTEGER NOT NULL,
+                  FOREIGN KEY(movie_id) REFERENCES movies(id)
+                  )",
+                 &[])
+        .unwrap();
+
+    conn.execute("CREATE TABLE IF NOT EXISTS crew (
+                  actor_id        INTEGER,
+                  movie_id        INTEGER,
+                  FOREIGN KEY(movie_id) REFERENCES movies(id),
+                  FOREIGN KEY(actor_id) REFERENCES actors(id)
                   )",
                  &[])
         .unwrap();

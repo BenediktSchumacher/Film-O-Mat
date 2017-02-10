842c8626daef78ebea6b39851b112bea9c24cf13
Benedikt Schumacher <bschumacher@uos.de>
2017-02-10 13:32:48 +0100

    added some tests

--- src/main.rs
+++ src/main.rs
@@ -14,4 +14,15 @@ fn main() {
     }
 
     import_movie("Batman", "2005");
+    add_genres("Batman", "2005", "Thriller");
+    add_rating("Batman", "2005", "8.324", 500);
+
+    let movie_1 = Movie {
+        title: "Batman".to_string(),
+        year: "2005".into(),
+    };
+
+    let name = "Heath Ledgers";
+
+    add_actor(name, vec![movie_1]);
 }

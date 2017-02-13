043f05fab7def4d63c957860a23e59669c8be380
Benedikt Schumacher <bschumacher@uos.de>
2017-02-13 18:03:19 +0100

    made things public

--- src/input/mod.rs
+++ src/input/mod.rs
@@ -6,7 +6,7 @@ use clap::{App, Arg};
 // This Struct is later given to our main program,
 // it contains all the needed information to make suggestions of films
 #[derive(Debug, Clone)]
-struct SearchParams {
+pub struct SearchParams {
     genre: Vec<String>,
     movies: Vec<String>,
     actors: Vec<String>,
@@ -24,7 +24,7 @@ impl SearchParams {
 }
 
 // Creates struct SearchParams in which all given parameters are included.
-fn get_search_params() -> SearchParams {
+pub fn get_search_params() -> SearchParams {
     // later given to main program, contains all arguments given in
     // command line
     let mut search_params = SearchParams::init();
@@ -44,11 +44,10 @@ fn get_search_params() -> SearchParams {
         .get_matches();
 
     // If a genre was given, save it in our SearchParams Type
-    if matches.is_present("genre"){
-        let genre =
-            match matches.value_of("genre") {
-                Some(genre) => genre,
-                None => panic!(),
+    if matches.is_present("genre") {
+        let genre = match matches.value_of("genre") {
+            Some(genre) => genre,
+            None => panic!(),
         };
         search_params.genre.push(genre.into());
         // Debug
@@ -56,11 +55,10 @@ fn get_search_params() -> SearchParams {
     }
 
     // If an actor was given, save it in our SearchParams Type
-    if matches.is_present("actor"){
-        let actor =
-            match matches.value_of("actor") {
-                Some(actor) => actor,
-                None => panic!(),
+    if matches.is_present("actor") {
+        let actor = match matches.value_of("actor") {
+            Some(actor) => actor,
+            None => panic!(),
         };
         search_params.actors.push(actor.into());
         // Debug
@@ -68,13 +66,13 @@ fn get_search_params() -> SearchParams {
     }
 
     // If a genre was given, save it in our SearchParams Type
-    if matches.is_present("movie"){
-        let movies:Vec<String> =
-            match matches.values_of("movie") {
-                Some(vals) => vals
-                    .map(|x| x.to_string())
-                    .collect(),
-                None => panic!(),
+    if matches.is_present("movie") {
+        let movies: Vec<String> = match matches.values_of("movie") {
+            Some(vals) => {
+                vals.map(|x| x.to_string())
+                    .collect()
+            }
+            None => panic!(),
         };
         search_params.movies = movies;
         // Debug
@@ -86,12 +84,3 @@ fn get_search_params() -> SearchParams {
     // return struct with all needed search_params
     search_params
 }
-
-// fn main() {
-    // Test
-    // possible test_input could be
-    // cargo run -- -a Name -m Lieblingsfilm1 Lieblingsfilm2 -g horror
-
-    // let args = get_search_params();
-    // println!("{:?}", args);
-// }

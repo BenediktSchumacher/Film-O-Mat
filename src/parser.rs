7f8d2df78aa2fb782fb9aeb2c13e7afeb4b09d17
Ludwig <amayadoa@gmail.com>
2017-02-13 14:15:15 +0100

    Modify parser functions for genre/rankings
    Parse string tested, but not with actual imdb-file

--- src/parser.rs
+++ src/parser.rs
@@ -3,86 +3,64 @@ use database::*;
 
 macro_rules! regex { ($re:expr) => { ::regex::Regex::new($re).unwrap() } }
 
+/// Represents a movie
 #[derive(Debug)]
-struct Film {
-    name: String,
-    votes: String,
-    rating: String,
-    year: String,
-}
+// Brauchen wir das struct überhaupt?
 
-impl Film {
-    fn new() -> Self {
-        Film {
-            name: String::new(),
-            votes: String::new(),
-            rating: String::new(),
-            year: String::new(),
-        }
-    }
-}
+// struct Film {
+//     name: String,
+//     votes: String,
+//     rating: String,
+//     year: String,
+// }
 
-#[derive(Debug)]
-struct Genre {
-    movie: String,
-    genre: String,
-}
-impl Genre {
-    fn new() -> Self {
-        Genre {
-            movie: String::new(),
-            genre: String::new(),
-        }
-    }
-}
+// impl Film {
+//     fn new() -> Self {
+//         Film {
+//             name: String::new(),
+//             votes: String::new(),
+//             rating: String::new(),
+//             year: String::new(),
+//         }
+//     }
+// }
+
+// #[derive(Debug)]
+// struct Genre {
+//     movie: String,
+//     genre: String,
+// }
+// impl Genre {
+//     fn new() -> Self {
+//         Genre {
+//             movie: String::new(),
+//             genre: String::new(),
+//         }
+//     }
+// }
 
 pub fn parse_rating(string: String) {
-    let mut all_films: Vec<Film> = Vec::new();
-
     let re = Regex::new(
         r"(\n [\s]+ [\d|\.]{10}[\s]+ (\d+)[\s]+ ([\d|\.]{3}) (.+) [(](\d{4})[)])"
-    ).unwrap();
+    ).unwrap(); //                    ^number     ^rating      ^title  ^year         
 
     for cap in re.captures_iter(string.as_str()) {
-        let mut film = Film::new();
-        // Debug
-        // println!("Name:{} Votecount:{} Rating:{} Year:{}", &cap[4], &cap[2], &cap[3], &cap[5] );
-        film.name.push_str(&cap[4]);
-        film.votes.push_str(&cap[2]);
-        film.rating.push_str(&cap[3]);
-        film.year.push_str(&cap[5]);
-        all_films.push(film);
+        add_rating(&cap[4], &cap[5], &cap[3], &cap[2]); 
     }
-
-    println!("{:?}", all_films[5]);
 }
 
 fn parse_genre(string: String) {
+    let re = Regex::new(
+        r"(\n(.+)[\s]+[(](\d{4})[)][\s]+(?:\(V\)|\(TV\))?[\s]*([^\{].+))"
+    ).unwrap(); //        ^year           ^(TV|V) filter             ^genre
+    // Series with form "NAME (YEAR)  {EPISODE INFO}   GENRE" are filtered out
 
-    // // hardcoded test file
-    // let mut f = File::open("test2");
-    // let mut buffer = String::new();
-    // // TODO: Error handling
-    // f.unwrap().read_to_string(&mut buffer);
-
-    let mut all_genres:Vec<Genre> = Vec::new();
-
-    let re = Regex::new(r"(\n(.+)[\s]+[(]\d{4}[)][\s]+(?:\(V\)|\(TV\))?[\s]*([^\{].+))").unwrap();
-    //                     ^ title     ^ year          ^ (TV|V) filter            ^ Genre
     for cap in re.captures_iter(string.as_str()) {
-
-        let mut genre = Genre::new();
-        // Debug
-        // println!("Name: {} Genre: {}", &cap[2], &cap[3] );
-        genre.movie.push_str(&cap[2]);
-        genre.genre.push_str(&cap[3]);
-        all_genres.push(genre);
+        add_rating(&cap[4], &cap[5], &cap[3], &cap[2]);
     }
-
-    // all_genres
-    // println!("{:?}", all_films);
 }
 
+// TODO: Filter series with form "NAME (YEAR)   {EPISODE INFO}"
 pub fn parse_movies(string: String) {
     // let re = Regex::new(r"(\n\u{0022}?[#|\+|\-|\*|\/|\.|\,|\!|\:|\&|\%|\$|\§|\w|\d|\s]*\u{0022}? \([\d]{4}\))").unwrap();
     let re = Regex::new("(\\n\u{0022}?[#|\\+|\\-|\\*|\\041|\\057|\\054|\\056|\\072|\\073|\\044|\\045|\\046|\\w|\\d|\\s]*\u{0022}? \\([\\d]{4}\\))")
@@ -96,4 +74,12 @@ pub fn parse_movies(string: String) {
         year = year.trim();
         import_movie(tmp[1], &year[1..5]);
     }
+}
+
+pub fn parse_actors() {
+    // TODO: implement it!
+}
+
+pub fn parse_directors() {
+    // TODO: implement it!
 }

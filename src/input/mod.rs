d47b8ef12cbbeb157c89a5f0fb5670398d8c30d5
ArnoStiefvater <arno.stiefvater@web.de>
2017-02-14 13:46:09 +0100

    exit() instead of panic!(), better error handling

--- src/input/mod.rs
+++ src/input/mod.rs
@@ -1,4 +1,5 @@
 use clap::{App, Arg};
+use std::process;
 
 // This Struct is later given to our main program,
 // it contains all the needed information to make suggestions of films
@@ -25,27 +26,31 @@ impl SearchParams {
 // Creates struct SearchParams in which all given parameters are included.
 pub fn get_search_params() -> SearchParams {
     // status of program
-    println!("[Getting Commandline Input]");
+    println!("Getting Commandline Input ...");
 
     // later given to main program, contains all arguments given in
     // command line
     let mut search_params = SearchParams::init();
 
     // our matches, we can later use those to handle our commandline input
-    let matches = App::new("FOM")
+    let matches = App::new("Film-O-Mat")
         .arg(Arg::with_name("genre")
             .short("g")
+            .help("Takes one or more genres")
             .takes_value(true)
             .multiple(true))
         .arg(Arg::with_name("actor")
             .short("a")
+            .help("Takes an actor")
             .takes_value(true))
         .arg(Arg::with_name("movie")
             .short("m")
+            .help("Takes one or more movies. e.g. \u{0022}Schindler's List\u{0022}")
             .takes_value(true)
             .multiple(true))
         .arg(Arg::with_name("rating")
             .short("r")
+            .help("Takes a rating")
             .takes_value(true))
         .get_matches();
 
@@ -56,7 +61,10 @@ pub fn get_search_params() -> SearchParams {
                 vals.map(|x| x.to_string())
                     .collect()
             }
-            None => panic!(),
+            None => {
+                println!("Invalid Genre");
+                process::exit(0);
+            }
         };
         search_params.genres = genres;
         // Debug
@@ -67,7 +75,10 @@ pub fn get_search_params() -> SearchParams {
     if matches.is_present("actor") {
         let actor = match matches.value_of("actor") {
             Some(actor) => actor,
-            None => panic!(),
+            None => {
+                println!("Invalid Actor");
+                process::exit(0);
+            }
         };
         search_params.actors.push(actor.into());
         // Debug
@@ -81,7 +92,10 @@ pub fn get_search_params() -> SearchParams {
                 vals.map(|x| x.to_string())
                     .collect()
             }
-            None => panic!(),
+            None => {
+                println!("Invalid Movie");
+                process::exit(0);
+            }
         };
         search_params.movies = movies;
         // Debug
@@ -91,12 +105,24 @@ pub fn get_search_params() -> SearchParams {
     if matches.is_present("rating") {
         let val = match matches.value_of("rating") {
             Some(vals) => vals,
-            None => panic!(),
+            None => {
+                println!("Invalid Rating");
+                process::exit(0);
+            }
         };
-        search_params.rating.push(val.parse().unwrap());
+        search_params.rating.push(
+            match val.parse() {
+                Ok(val) => val,
+                Err(err) => {
+                    println!("{:?}", err);
+                    process::exit(0);
+                }
+            }
+        );
         // Debug
         // println!("Rating: {:?}", search_params.rating);
     }
+
     // Debug
     // println!("{:?}", search_params);
     search_params

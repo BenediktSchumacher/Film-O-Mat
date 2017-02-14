454e18a2521253535cbb17e5956f553ee9e48abb
Ludwig <amayadoa@gmail.com>
2017-02-14 13:43:55 +0100

    Add documentation
    Also update some comments

--- src/parser.rs
+++ src/parser.rs
@@ -1,12 +1,18 @@
 use regex::Regex;
 use database::*;
 
+/// Parser for a string of many movies and their ratings.
+/// For movie identity the year parsed. To filter movies with too less ratings,
+/// the number of ratings is parsed, too.
+/// Series are marked with quotation marks. Series are not encountered. All 
+/// entries with title "[Series Title]" are filtered out.
 pub fn parse_rating(string: String) {
-    // let re = Regex::new(r"(\n [\s]+ [\d|\.]{10}[\s]+ (\d+)[\s]+ ([\d|\.]{3}) (.+) [(](\d{4})[)])")
+    // (old regex, can be deleted later)let re = Regex::new(r"(\n [\s]+ [\d|\.]{10}[\s]+ 
+    // (\d+)[\s]+ ([\d|\.]{3}) (.+) [(](\d{4})[)])")
     let re = Regex::new("(\\n [\\s]+ [\\d|\\.]{10}[\\s]+ (\\d+)[\\s]+ \
                          ([\\d|\\.]{3})[\\s]{2}([^\u{0022}]{1}.+) \\(([\\d]{4})\\))")
-        .unwrap(); //                    ^number     ^rating      ^title  ^year
-
+        .unwrap();  //                                    ^number of ratings          
+                    //     ^rating                            ^title   ^year 
     println!("{:?}", &re);
 
     for cap in re.captures_iter(string.as_str()) {
@@ -20,12 +26,15 @@ pub fn parse_rating(string: String) {
     }
 }
 
+/// Parser for a string of movies and their genres.
+/// Again for movie identity the year is parsed, too. Series with title
+/// "[Series Title]" are filtered out. 
 pub fn parse_genre(string: String) {
     // Series starting with "quotation marks" are filtered out
     let re = Regex::new("(\\n([^\u{0022}].*)[\\s]+[\\(](\\d{4})[\\)][\\s]+(?:\
                          \\(V\\)|\\(TV\\))?[\\s]*([^\\{].+))")
-        .unwrap();
-    //                ^title             ^year               ^filter for (V)/(TV)           ^genre
+        .unwrap();  //                   ^title         ^year 
+                    //   ^filter for (V)/(TV)           ^genre         ^genre
     for cap in re.captures_iter(string.as_str()) {
         add_genres(&cap[2], &cap[3], &cap[4]);
     }
